d3a235ba8043e0ba2f34bf8420811e1fbb05def5
sreerag <sreerag9121@gmail.com>
2025-08-25 18:42:02 +0530

    edit home page 1

--- lib/views/Home/home_page.dart
+++ lib/views/Home/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
-import 'package:token_management_user/controller/auth_contoller.dart';
 import 'package:token_management_user/core/widgets/my_text_field.dart';
 
 class HomePage extends StatelessWidget {
